7e8ec5ca082ef23ff9a24572f8c94cc0e12dde45
Nov Matake <nov@matake.jp>
2018-06-20 15:07:29 +0900

    Initial Commit

---
+++ WebViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewApp
+//
+//  Created by 真武　信和 on 2018/06/20.
+//  Copyright © 2018年 真武　信和. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
