b7570ff913099f21bcf652eddba07366be7a9d87
Nov Matake <nov@matake.jp>
2018-06-27 12:29:16 +0900

    add WebView

--- WebViewApp/ViewController.swift
+++ WebViewApp/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url_string = "https://ios-autofill.self-issued.app"
+        let url = NSURL(string: url_string)
+        let req = NSURLRequest(url: url! as URL)
+        self.webView.load(req as URLRequest)
     }
 
 
